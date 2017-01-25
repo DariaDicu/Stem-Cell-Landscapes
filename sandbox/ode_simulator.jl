1c06953bb63bcc2976b8449ba025d0dfc3782a84
IvanVeleslavov <ivanveleslavov@gmail.com>
2017-01-25 10:20:34 +0000

    Updated Daria's original ode to include option to monitor only end points, instead of full trajectory

--- sandbox/ode_simulator.jl
+++ sandbox/ode_simulator.jl
@@ -63,12 +63,13 @@ function build_landscape(N::Int64, F::Function, n::Int64, bounds)
   end
 
   # Convert matrix to DataFrame.
-  return (convert(DataFrame,output))
+  return (convert(DataFrame,output)), N, n
 end
 
 # Example of function for representing a 2 transcription-factor with self- and
 # mutual- regulation. Parameters are hardcoded in the function. See Wang et al,
 # 2011 (http://www.pnas.org/content/108/20/8257.full).
+#F = a -> function (t,x)
 F = function (t,x)
   a = 0.3
   n = 4
@@ -82,10 +83,28 @@ F = function (t,x)
 end
 
 # Code to plot a contour map for the cell-fate ODE represented by F.
-data = build_landscape(1000, F, 2, (0,5))
+data, N, n = build_landscape(100, F, 2, (-5,5))
 
-X = convert(Array{Float64},deepcopy(data[2]));
-Y = convert(Array{Float64},deepcopy(data[3]));
+#Toggle between rest only=true if only want to plot the end positions,
+#or rest_only=false to consider full trajectory
+rest_only = false
+
+if rest_only
+  #Find the resting values for each run
+  resting_values = zeros(N, n)
+
+  for run = 1:N
+    data_edit = data[data[size(data)[2]] .== run, :]
+    for dim = 1:n
+      resting_values[run, dim] = data_edit[findmax(data_edit[1])[2],dim+1]
+    end
+  end
+  X = convert(Array{Float64},deepcopy(resting_values[:,1]));
+  Y = convert(Array{Float64},deepcopy(resting_values[:,2]));
+else
+  X = convert(Array{Float64},deepcopy(data[2]));
+  Y = convert(Array{Float64},deepcopy(data[3]));
+end
 
 dens1 = kde((X, Y))
 dens2=1e-23*ones(size(dens1.density))+dens1.density
@@ -93,6 +112,7 @@ dens2=1e-23*ones(size(dens1.density))+dens1.density
 ldens=-log(dens2);
 ldens=ldens-maximum(ldens)
 
+gr()
 contour_plot=contour(dens1.x,dens1.y,dens1.density,levels=100,
   legend=false,xlabel="Dim 1",ylabel="Dim 2")
 plot(contour_plot)
