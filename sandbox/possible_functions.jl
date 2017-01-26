4eddaf71cc42f575c95ed5d453b44546bddfd090
IvanVeleslavov <ivanveleslavov@gmail.com>
2017-01-26 10:41:07 +0000

    Introduced toy function

--- sandbox/possible_functions.jl
+++ sandbox/possible_functions.jl
@@ -60,6 +60,7 @@ F_sub_pitch = alpha -> function (t,x)
   return [F1(x[1], x[2]), F2(x[1], x[2])]
 end
 
+#Supercritical Hopf bifurcation
 F_super_hopf = alpha -> function (t,x)
   #alpha = 0
 
@@ -70,6 +71,7 @@ F_super_hopf = alpha -> function (t,x)
   return [F1(x[1], x[2]), F2(x[1], x[2])]
 end
 
+#Subcritical Hopf bifurcation
 F_sub_hopf = alpha -> function (t,x)
   #alpha = 0
 
@@ -77,5 +79,17 @@ F_sub_hopf = alpha -> function (t,x)
     -x2 + (x1 * (alpha +((x1^2)+(x2^2))))
   F2 = (x1, x2) ->
     x1 + (x2 * (alpha +((x1^2)+(x2^2))))
+  return [F1(x[1], x[2]), F2(x[1], x[2])]
+end
+
+F_toy_system = function (t,x)
+  a = 1
+  b = 1
+  g = 1
+
+  F1 = (x1, x2) ->
+   (a * x1) - (b * x1) - (g * x2)
+  F2 = (x1, x2) ->
+   b*x1
   return [F1(x[1], x[2]), F2(x[1], x[2])]
 end
