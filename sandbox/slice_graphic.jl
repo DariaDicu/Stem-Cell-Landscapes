c048dcffdb80129badfcd2c66278fa044378f566
IvanVeleslavov <ivanveleslavov@gmail.com>
2017-02-17 16:19:55 +0000

    Generalised for n slices

--- sandbox/slice_graphic.jl
+++ sandbox/slice_graphic.jl
@@ -1,24 +1,14 @@
 using Plots
 
-my_plot = scatter(0,0)
+# How many slices do you want
+slices = 5
 
-plotly(0,0)
+interval = Integer(floor(length(ldens)[2]/slices))
 
-for i = 0:5
-  Plots.plot(ldens[(i*50)+1,:])
+for i = 0:slices-1
+  Plots.plot(ldens[(i*interval)+1,:], linecolor = i+1, linewidth=4)
   scatter!(0,-22)
   xlabel!("X")
   ylabel!("'Potential'")
   Plots.savefig(string(i)*".png")
 end
-
-
-plot!(ldens[50,:])
-
-plot!(ldens[100,:])
-
-plot!(ldens[150,:])
-
-plot!(ldens[200,:])
-
-plot!(ldens[250,:])
